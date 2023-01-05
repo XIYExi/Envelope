d24e84e44b906b655ec1bc04c860363d8c733388
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-05 18:01:49 +0800

    编写tool工具文件

---
+++ src/core-utils/tool.ts
@@ -0,0 +1,44 @@
+export const isDev = process.env.NODE_ENV === 'development';
+
+// 生成uuid
+export function uuid(len: number, radix: number) {
+  let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  let uuid = [],
+    i;
+  radix = radix || chars.length;
+
+  if (len) {
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
+  } else {
+    let r;
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16);
+        uuid[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r];
+      }
+    }
+  }
+
+  return uuid.join('');
+}
+
+
+export function getBase64(file: File | Blob) {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = error => reject(error);
+  });
+}
+
+
+
+
+
+
+
+
