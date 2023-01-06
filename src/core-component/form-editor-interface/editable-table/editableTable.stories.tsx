32a3a6e7e47f05133f38af2d945d844667ae44fe
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-06 15:11:39 +0800

    提交可编辑表单

---
+++ src/core-component/form-editor-interface/editable-table/editableTable.stories.tsx
@@ -0,0 +1,14 @@
+import EditableTable from './index';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default{
+  title: 'core-component / editable-table',
+  component: EditableTable
+} as ComponentMeta<typeof EditableTable>;
+
+const Template:ComponentStory<typeof EditableTable> = (args) => <EditableTable {...args} />
+
+export const SampleEditableTable = Template.bind({});
+SampleEditableTable.args = {
+
+}
