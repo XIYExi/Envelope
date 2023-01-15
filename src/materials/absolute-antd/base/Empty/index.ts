3ffb40a09225ed6fd588d9403342c8099bf33617
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-15 12:42:04 +0800

    提交antd-absolute-empty

---
+++ src/materials/absolute-antd/base/Empty/index.ts
@@ -0,0 +1,5 @@
+import AEmpty from './Empty';
+import schema from './schema';
+import template from './template';
+
+export {AEmpty, schema, template};
---
+++ src/materials/absolute-antd/base/Empty/schema.ts
@@ -0,0 +1,35 @@
+import { ITextConfigType } from '@/core-component/type';
+
+
+export type TEmptyEditData = Array<ITextConfigType>;
+
+export interface IEmptyConfig {
+  description: string;
+  image: string;
+}
+
+export interface IEmptySchema {
+  editData: TEmptyEditData;
+  config: IEmptyConfig;
+}
+
+const Empty: IEmptySchema = {
+  editData: [
+    {
+      key: 'description',
+      name: '内容描述',
+      type: 'Text'
+    },
+    {
+      key: 'image',
+      name: '设置显示图片',
+      type: 'Text'
+    }
+  ],
+  config: {
+    description: '暂无数据',
+    image: 'https://s1.ax1x.com/2023/01/15/pSQdf8U.png',
+  }
+}
+
+export default Empty;
