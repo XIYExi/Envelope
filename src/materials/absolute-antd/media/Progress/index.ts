4c64b732e4dbcde3b11a414f8862ad63137891da
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-22 16:56:39 +0800

    提交antd-absolute-progress

---
+++ src/materials/absolute-antd/media/Progress/index.ts
@@ -0,0 +1,5 @@
+import AProgress from './Progress';
+import schema from './schema';
+import template from './template';
+
+export {AProgress,schema,template};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 import styled from 'styled-components';
 import 'antd/dist/antd.css';
 import React from 'react';
-import { ASwitch, schema } from '@/materials/absolute-antd/control/Switch';
+import { AProgress, schema } from '@/materials/absolute-antd/media/Progress';
 
 const Head = styled.h2`
   font-size: 50px;
@@ -15,9 +15,8 @@ export default function IndexPage() {
       <Head>dada</Head>
       <h1 className={styles.title}>Page index</h1>
 
-      <ASwitch isTpl={false}
+      <AProgress isTpl={false}
                {...schema.config}
-        onChange={e=>console.log('index: '+e)}
       />
 
     </div>
