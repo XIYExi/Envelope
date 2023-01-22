2bec350bf9de352e2f00a0bf642dcf201880e658
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-22 20:02:47 +0800

    提交antd-absolute-comment

---
+++ src/materials/absolute-antd/social/Comment/index.ts
@@ -0,0 +1,5 @@
+import AComment from './Comment';
+import schema from './schema';
+import template from './template';
+
+export {AComment,template,schema};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 import styled from 'styled-components';
 import 'antd/dist/antd.css';
 import React from 'react';
-import { ACollapse, schema } from '@/materials/absolute-antd/social/Collapse';
+import { AComment, schema } from '@/materials/absolute-antd/social/Comment';
 
 const Head = styled.h2`
   font-size: 50px;
@@ -15,7 +15,7 @@ export default function IndexPage() {
       <Head>dada</Head>
       <h1 className={styles.title}>Page index</h1>
 
-      <ACollapse isTpl={false}
+      <AComment isTpl={false}
                {...schema.config}
       />
 
