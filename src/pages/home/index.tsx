7eb77991a1d969e995b5fffbc1d938ff00d3037a
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-28 12:33:30 +0800

    开始编写界面

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.less';
+import { Tabs, message } from 'antd';
+import { history } from 'umi';
+import {
+  MobileOutlined,
+  ConsoleSqlOutlined,
+  CodeOutlined,
+  IdcardOutlined,
+} from '@ant-design/icons';
+
+
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import styles from './index.less';
 import styled from 'styled-components';
 import 'antd/dist/antd.css';
 import React from 'react';
-import { AStatistic, schema } from '@/materials/absolute-antd/social/Statistic';
 
 const Head = styled.h2`
   font-size: 50px;
@@ -14,10 +13,6 @@ export default function IndexPage() {
     <div>
       <Head>dada</Head>
       <h1 className={styles.title}>Page index</h1>
-
-      <AStatistic isTpl={false}
-               {...schema.config}
-      />
 
     </div>
   );
