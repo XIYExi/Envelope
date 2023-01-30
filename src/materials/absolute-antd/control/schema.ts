d8422fb7b0b81235ae6b379147a6230b88fbd60d
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-01-30 14:10:05 +0800

    提交antd-control入口文件

---
+++ src/materials/absolute-antd/control/schema.ts
@@ -0,0 +1,19 @@
+import Button from './Button/schema';
+import CheckBox from './CheckBox/schema';
+import Radio from './Radio/schema';
+import Segmented from './Segmented/schema';
+import Select from './Select/schema';
+import Slider from './Slider/schema';
+import Switch from './Switch/schema';
+
+const antdControlSchema = {
+  Button,
+  CheckBox,
+  Radio,
+  Segmented,
+  Select,
+  Slider,
+  Switch
+}
+
+export default antdControlSchema;
