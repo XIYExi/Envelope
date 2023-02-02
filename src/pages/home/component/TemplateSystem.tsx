10198aadac506cccef6d231a3c4ae5f61102edae
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-02-02 18:00:07 +0800

    提交模板选择组件

--- src/pages/home/component/TemplateSystem.tsx
+++ src/pages/home/component/TemplateSystem.tsx
@@ -1,24 +1,32 @@
 import React,{FC} from 'react';
 import styles from '@/pages/home/index.less';
-import { Card, Space } from 'antd';
+import { Button, Card, Space, Typography } from 'antd';
 
 
 const TemplateSystem:FC<{
   template: string;
   chooseTemplate:(e:string)=>void;
+  prevStep: ()=>void;
+  nextStep:()=>void;
 }> = props => {
 
-  const {template, chooseTemplate} = props;
+  const {
+    template,
+    chooseTemplate,
+    prevStep,
+    nextStep
+  } = props;
 
   return(
     <div>
       <div className={styles.operation}>
-        <Space wrap>
+        <Space wrap size={30}>
           <Card
             hoverable
             style={{
               width: 240,
-              borderColor: template === 'empty' ? '#8db8ee' : ''
+              borderColor: template === 'empty' ? '#8db8ee' : '',
+              margin: '5px'
             }}
             cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
             onClick={()=>chooseTemplate('empty')}
@@ -28,14 +36,15 @@ const TemplateSystem:FC<{
 
           {
             //TODO 后续从服务器拉取数据，这里通过数组先填充
-            new Array(12).fill(0).map((item, i) => {
+            new Array(10).fill(0).map((item, i) => {
               return (
                 <Card
                   key={i}
                   hoverable
                   style={{
                     width: 240,
-                    borderColor: template === 'test' ? '#8db8ee' : ''
+                    borderColor: template === 'test' ? '#8db8ee' : '',
+                    margin: '5px'
                   }}
                   cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
                   onClick={()=>chooseTemplate('empty')}
@@ -45,10 +54,30 @@ const TemplateSystem:FC<{
               )
             })
           }
-
         </Space>
-
       </div>
+
+      <Typography.Paragraph style={{
+        marginTop: '20px'
+      }}>
+        <Button
+          size={'large'}
+          style={{
+            width: '120px',
+            borderRadius: '20px',
+            marginRight: '30px'
+          }}
+          onClick={()=>prevStep()}
+        >Previous</Button>
+
+        <Button
+          type={'primary'}
+          size={'large'}
+          style={{width: '120px', borderRadius: '20px'}}
+          onClick={()=>nextStep()}
+        >Next</Button>
+      </Typography.Paragraph>
+
     </div>
   )
 }
