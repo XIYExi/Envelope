85528e8bd6eb036b2c1ec4ae99891415821e9acd
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-02-02 17:59:37 +0800

    提交UI系统选择组件

--- src/pages/home/component/UISystem.tsx
+++ src/pages/home/component/UISystem.tsx
@@ -1,21 +1,28 @@
 import React, { FC } from 'react';
 import styles from '@/pages/home/index.less';
-import { MobileOutlined } from '@ant-design/icons';
+import { AntDesignOutlined, MobileOutlined, RightOutlined } from '@ant-design/icons';
 import { Image } from 'semantic-ui-react';
-
-
+import { Button, Card, Space, Typography } from 'antd';
+import './index.less';
+import antd from '../../../assets/home/antd.svg';
+import antdLogo from '../../../assets/home/antdlogo.svg';
+import sem from '../../../assets/home/sem.svg';
+import semLogo from '../../../assets/home/semlogo.png';
+import lole from '../../../assets/home/lole.svg';
+import loleLogo from '../../../assets/home/loleLogo.png';
 
 const UISystem:FC<{
   ui: string;
   chooseUI: (e:string)=>void;
+  nextStep: () => void;
 }> = props => {
 
-  const {ui, chooseUI} = props;
+  const {ui, chooseUI, nextStep} = props;
 
   return(
     <>
-      <div >
-        <div className={styles.operation}>
+      <Space size={30}>
+        <div className={styles.operation} style={{paddingRight: '50px'}}>
           <div onClick={()=>chooseUI('antd')}
                style={{marginBottom: '15px'}}
           >
@@ -24,8 +31,13 @@ const UISystem:FC<{
                    borderColor: ui === 'antd' ? '#8db8ee' : ''
                  }}
             >
-              <Image />
-              <div>Ant Design</div>
+              <Space className='ui-sys-space' size={90} align='center'>
+                <Image width={48} src={antdLogo} />
+                <div style={{margin: 0}}>
+                  Ant Design
+                  <RightOutlined style={{marginLeft:'20px'}}/>
+                </div>
+              </Space>
             </div>
           </div>
 
@@ -37,8 +49,13 @@ const UISystem:FC<{
                    borderColor: ui === 'semantic' ? '#8db8ee' : ''
                  }}
             >
-              <MobileOutlined />
-              <div>Semantic UI</div>
+              <Space className='ui-sys-space' size={90} align='center'>
+                <Image width={48} src={semLogo} />
+                <div style={{margin: 0}}>
+                  Semantic UI
+                  <RightOutlined style={{marginLeft:'20px'}}/>
+                </div>
+              </Space>
             </div>
           </div>
 
@@ -50,14 +67,63 @@ const UISystem:FC<{
                    borderColor: ui === 'lole' ? '#8db8ee' : ''
                  }}
             >
-              <MobileOutlined />
-              <div>Love Letter UI</div>
+              <Space className='ui-sys-space' size={90} align='center'>
+                <Image width={48} src={loleLogo} />
+                <div style={{margin: 0}}>
+                  Love Letter
+                  <RightOutlined style={{marginLeft:'20px'}}/>
+                </div>
+              </Space>
             </div>
           </div>
 
         </div>
 
-      </div>
+        {/*右侧选择卡片*/}
+        <div className='ui-sys-card'>
+          {
+            ui === 'antd' &&
+              <React.Fragment>
+                <Image preview={false} src={antd} alt='' width={300} height={350}/>
+                <Typography.Paragraph className='ui-sys-text'>
+                  Stacks is a production-ready library of stackable content blocks built in React Native
+                </Typography.Paragraph>
+              </React.Fragment>
+          }
+
+          {
+            ui === 'semantic' &&
+            <React.Fragment>
+              <Image preview={false} src={sem} alt='' width={300} height={350}/>
+              <Typography.Paragraph className='ui-sys-text'>
+                Stacks is a production-ready library of stackable content blocks built in React Native
+              </Typography.Paragraph>
+            </React.Fragment>
+          }
+
+          {
+            ui === 'lole' &&
+            <React.Fragment>
+              <Image preview={false} src={lole} alt='' width={300} height={350}/>
+              <Typography.Paragraph className='ui-sys-text'>
+                Stacks is a production-ready library of stackable content blocks built in React Native
+              </Typography.Paragraph>
+            </React.Fragment>
+          }
+
+          <Typography.Paragraph>
+            <Button
+              type={'primary'}
+              size={'large'}
+              style={{width: '120px', borderRadius: '20px'}}
+              onClick={()=>nextStep()}
+            >Next</Button>
+          </Typography.Paragraph>
+
+        </div>
+
+
+      </Space>
     </>
   )
 }
