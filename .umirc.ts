e750974e7f049f8f13909a8b90776ecbbe12633c
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-02-07 14:37:17 +0800

    @babel/generator也需要约束版本！！！

--- .umirc.ts
+++ .umirc.ts
@@ -27,4 +27,14 @@ export default defineConfig({
     },
   ],
   fastRefresh: {},
+  externals: {
+    react: 'var window.React',
+    'react-dom': 'var window.ReactDOM',
+    'prop-types': 'var window.PropTypes',
+    '@alifd/next': 'var window.Next',
+    '@alilc/lowcode-engine': 'var window.AliLowCodeEngine',
+    '@alilc/lowcode-engine-ext': 'var window.AliLowCodeEngineExt',
+    moment: 'var window.moment',
+    lodash: 'var window._',
+  },
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,27 @@
  * ref: https://github.com/umijs/umi/issues/1109#issuecomment-423380125
  */
 
+/**
+ * @babel/core依赖问题
+ * https://stackoverflow.com/questions/69343038/cant-import-the-named-export-xxxx-from-non-ecmascript-module-only-default-expo
+ * https://www.bookstack.cn/read/alibaba-lowcode-engine-1.0-zh/9515333ef37f0c9b.md
+ * 配置貌似没用，但是放着不影响
+ */
+
 module.exports = {
   resolve: {
+    extensions: ['*', '.mjs', '.js', '.json'],
     alias: {
       '@': require('path').resolve(__dirname, 'renderer'),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+    ],
   },
 };
