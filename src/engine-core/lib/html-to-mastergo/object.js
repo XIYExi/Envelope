3a5fdedbd11951b7fab9cd84528a38e9d7390693
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-03-22 15:31:34 +0800

    调教解析器依赖lib

---
+++ src/engine-core/lib/html-to-mastergo/object.js
@@ -0,0 +1,6 @@
+export function size(obj) {
+  return Object.keys(obj).length;
+}
+
+export const fastClone = (data) =>
+  typeof data === 'symbol' ? null : JSON.parse(JSON.stringify(data));
---
+++ src/engine-core/lib/inject.js
@@ -0,0 +1,7 @@
+import { htmlToMastergo } from './html-to-mastergo';
+
+const handleButton = () => {
+  const layers = htmlToMastergo('body', true);
+  let json = JSON.stringify({ layers });
+  console.log(json);
+};
