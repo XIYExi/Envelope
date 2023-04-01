c4f3a7c166e7e83f77c49f1b55617c894e2c034a
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-04-01 11:18:23 +0800

    对动态引擎进行修改，可以动态加载不同的ui系统

--- src/engine-lib-absolute/core/DynamicEngine.tsx
+++ src/engine-lib-absolute/core/DynamicEngine.tsx
@@ -1,31 +1,25 @@
-import {dynamic} from 'umi';
+import { dynamic } from 'umi';
 import React, { FC, memo, useMemo } from 'react';
 import { Loader } from 'semantic-ui-react';
 
-
-
 // Ant Design UI React 组件类型
-export type componentsType = 'base' |
-  'control' |
-  'media' |
-  'social';
+export type componentsType = 'base' | 'control' | 'media' | 'social';
 
 export interface DynamicType {
   isTpl: boolean;
-  config: { [key : string]: any };
+  config: { [key: string]: any };
   type: string;
   componentsType: componentsType;
   category: string;
+  ui: string;
 }
 
-const DynamicFunc = (
-  type: string,
-  componentsType: string,
-  ui: string = 'semantic-ui-react'
-) => {
+const DynamicFunc = (type: string, componentsType: string, ui: string) => {
   return dynamic({
-    loader: async function() {
-      const { default: Graph } = await import(`@/materials/absolute-antd/${componentsType}/${type}`);
+    loader: async function () {
+      const { default: Graph } = await import(
+        `@/materials/absolute-${ui}/${componentsType}/${type}`
+      );
       const Component = Graph;
       return (props: DynamicType) => {
         const { config, isTpl } = props;
@@ -34,25 +28,22 @@ const DynamicFunc = (
     },
     loading: () => (
       <div>
-        <Loader active inline='centered' >a</Loader>
+        <Loader active inline="centered">
+          a
+        </Loader>
       </div>
     ),
-  })
-}
-
+  });
+};
 
 const DynamicEngine = memo((props: DynamicType) => {
-  const {
-    type,
-    config,
-    category
-  } = props;
+  const { type, config, category, ui } = props;
 
   const Dynamic = useMemo(() => {
-    return (DynamicFunc(type, category) as unknown) as FC<DynamicType>;
+    return DynamicFunc(type, category, ui) as unknown as FC<DynamicType>;
   }, [config]);
 
-  return <Dynamic {...props}/>;
+  return <Dynamic {...props} />;
 });
 
 export default DynamicEngine;
