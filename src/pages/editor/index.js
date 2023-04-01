f08eabcdc892e4bba54c5df56fc433e7177a2755
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-04-01 11:18:08 +0800

    absolute-lowcode的入口添加渲染配置，可以选择使用不同的ui系统

--- src/pages/editor/index.js
+++ src/pages/editor/index.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
 import Container from './Container';
 
+import SemanticContainer from './SemanticContainer';
+
 import styles from './Container.less';
 
+import 'semantic-ui-css/semantic.min.css';
+
 function BasicLayout(props) {
+  useEffect(() => {
+    console.log(props.location.query.ui);
+  }, []);
+
+  const uiSys = props.location.query.ui ? props.location.query.ui : 'antd';
   return (
     <div className={styles.layout}>
       <DndProvider backend={HTML5Backend}>
-        <Container {...props} />
+        {uiSys === 'antd' && <Container {...props} />}
+        {uiSys === 'semantic' && <SemanticContainer {...props} />}
       </DndProvider>
     </div>
   );
