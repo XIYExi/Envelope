af9eda6066293cc0bc58ace126656e739ed012c7
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-04-22 10:52:33 +0800

    优化了组件预览侧边栏

--- src/engine-lib-antv/components/NodesBar.tsx
+++ src/engine-lib-antv/components/NodesBar.tsx
@@ -8,8 +8,15 @@ import React, {
 } from 'react';
 import { ActionType } from '@/engine-lib-antv/common/enums';
 import {
+  getALGONode,
+  getBorderedRectNode,
+  getConnectCircleNode,
+  getDagNode,
+  getDataRectNode,
   getDiamondNode,
   getEllipseNode,
+  getERNode,
+  getPRONode,
   getRectNode,
   getVueNode,
 } from '../common/transform';
@@ -73,16 +80,33 @@ const NodesBar: FC<any> = (props) => {
     //d = _d;
   }, [props]);*/
 
-  // 开始拖拽函数，通过监听组件内部的ref，来判断是否需要更新函数
-  // 因为函数中依赖dnd和graph的最新值，为了避免重复渲染，使用useCallBack监听，只要不拖拽创建新的节点就不会触发更新
+  /**
+   * 开始拖拽函数，通过监听组件内部的ref，来判断是否需要更新函数
+   * 因为函数中依赖dnd和graph的最新值，为了避免重复渲染，使用useCallBack监听，只要不拖拽创建新的节点就不会触发更新
+   * @important
+   * 拖拽函数中生成的是antv x6中的node对象！
+   * 其工作原理这个时候和nodeBar.ts中的BaseStyle没用关系！
+   */
   const startDrag = useCallback(
     (currentTarget: any, e: any) => {
       //console.log('initDnd', props.dnd)
       //console.log('尝试得到 graph.value ',props.graph.current)
-
       //console.log('startDrag', currentTarget, e)
+
       const { actionType, shape, label } = currentTarget;
-      const { TRIGGER, CONDITION, ACTION } = ActionType;
+      const {
+        TRIGGER,
+        CONDITION,
+        ACTION,
+        OPTIONAL,
+        DATA,
+        CONNECT,
+        DAG,
+        DAGLINE,
+        ER,
+        ALGO,
+        PRO,
+      } = ActionType;
       let json;
       switch (actionType) {
         // 触发器
@@ -115,6 +139,71 @@ const NodesBar: FC<any> = (props) => {
             initialization: true,
           });
           break;
+        // 可选操作
+        case OPTIONAL:
+          json = getBorderedRectNode({
+            shape,
+            tooltip: label,
+            size: { width: 100, height: 50 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        // 数据节点
+        case DATA:
+          json = getDataRectNode({
+            shape: 'polygon',
+            tooltip: label,
+            size: { width: 100, height: 50 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        case CONNECT:
+          json = getConnectCircleNode({
+            shape: 'circle',
+            tooltip: label,
+            size: { width: 60, height: 60 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        case DAG:
+          json = getDagNode({
+            shape: 'dag-node',
+            tooltip: label,
+            size: { width: 350, height: 60 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        case ALGO:
+          json = getALGONode({
+            shape: 'dag-node',
+            tooltip: label,
+            size: { width: 350, height: 60 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        case PRO:
+          json = getPRONode({
+            shape: 'dag-node',
+            tooltip: label,
+            size: { width: 350, height: 60 },
+            actionType,
+            initialization: true,
+          });
+          break;
+        case ER:
+          json = getERNode({
+            shape: 'er-rect',
+            tooltip: label,
+            size: { width: 150, height: 24 },
+            actionType,
+            initialization: true,
+          });
+          break;
         case 'Vue-node':
           json = getVueNode({
             shape: 'rect',
@@ -123,6 +212,8 @@ const NodesBar: FC<any> = (props) => {
             actionType,
             initialization: true,
           });
+          break;
+        case 'Dag-Node':
           break;
         default:
           break;
@@ -166,7 +257,7 @@ const NodesBar: FC<any> = (props) => {
       return (
         <>
           <TabPane tab={generateHeader('base', '')} key="1">
-            <div className={'ctitle'}>基础组件</div>
+            <div className={'ctitle'}>基础流程图组件</div>
             {nodes.map((node: any, index: number) => {
               return (
                 <TargetBoxAntV
@@ -174,6 +265,11 @@ const NodesBar: FC<any> = (props) => {
                   onMouseDown={(e: any) => startDrag(node, e)}
                   title={node.title}
                 >
+                  {/**
+                   底下这段div用于绘制预览流程图图形，它只是一个有颜色和border的div，通过baseStyle控制，
+                   并不是antv x6中的node对象！
+                   其原理和原生引擎中使用图片代替组件预览侧边中的预览组件是一样的！
+                   */}
                   <div
                     className={
                       data.freeze
@@ -185,14 +281,18 @@ const NodesBar: FC<any> = (props) => {
                 </TargetBoxAntV>
               );
             })}
-            <div className={'ctitle'}>自定义组件</div>
+
+            {/**DAG自定义图*/}
+            <div className={'ctitle'}> DAG 图</div>
+
             <TargetBoxAntV
+              title={'算法流程'}
               onMouseDown={(e: any) =>
                 startDrag(
                   {
-                    label: 'React-Node',
-                    shape: 'rect',
-                    actionType: 'React-Node',
+                    label: 'algro-node',
+                    shape: 'dag-node',
+                    actionType: 'ALGO',
                   },
                   e,
                 )
@@ -200,6 +300,72 @@ const NodesBar: FC<any> = (props) => {
             >
               <div>
                 <ReactNode
+                  src="https://s1.ax1x.com/2023/04/19/p9i5MPx.png"
+                  className={data.freeze ? `freeze-rect` : `static-react-node`}
+                />
+              </div>
+            </TargetBoxAntV>
+
+            <TargetBoxAntV
+              title={'工程建模'}
+              onMouseDown={(e: any) =>
+                startDrag(
+                  {
+                    label: 'dag-node',
+                    shape: 'dag-node',
+                    actionType: 'PRO',
+                  },
+                  e,
+                )
+              }
+            >
+              <div>
+                <ReactNode
+                  src="https://s1.ax1x.com/2023/04/19/p9i5MPx.png"
+                  className={data.freeze ? `freeze-rect` : `static-react-node`}
+                />
+              </div>
+            </TargetBoxAntV>
+
+            <TargetBoxAntV
+              title={'人工智能'}
+              onMouseDown={(e: any) =>
+                startDrag(
+                  {
+                    label: 'dag-node',
+                    shape: 'dag-node',
+                    actionType: 'DAG',
+                  },
+                  e,
+                )
+              }
+            >
+              <div>
+                <ReactNode
+                  src="https://s1.ax1x.com/2023/04/19/p9i5MPx.png"
+                  className={data.freeze ? `freeze-rect` : `static-react-node`}
+                />
+              </div>
+            </TargetBoxAntV>
+
+            {/**E-R*/}
+            <div className={'ctitle'}>E - R 关系图</div>
+            <TargetBoxAntV
+              title={'E - R'}
+              onMouseDown={(e: any) =>
+                startDrag(
+                  {
+                    label: 'er-node',
+                    shape: 'er-node',
+                    actionType: 'ER',
+                  },
+                  e,
+                )
+              }
+            >
+              <div>
+                <ReactNode
+                  src="https://s1.ax1x.com/2023/04/19/p9i5MPx.png"
                   className={data.freeze ? `freeze-rect` : `static-react-node`}
                 />
               </div>
