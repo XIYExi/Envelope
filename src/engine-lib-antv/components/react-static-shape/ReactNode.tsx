09593e3741a67c0f60832e6dd9c16f06e1eb0bac
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-04-22 10:52:47 +0800

    静态react节点

--- src/engine-lib-antv/components/react-static-shape/ReactNode.tsx
+++ src/engine-lib-antv/components/react-static-shape/ReactNode.tsx
@@ -1,26 +1,27 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
+/**此节点用于展示自定义节点的预览图*/
+
 const Container = styled.div`
-  height: 50px;
-  width: 75%;
-  display: flex;
+  height: 65px;
+  width: 100%;
   user-select: none;
-  background-color: #ff8228;
-  border: 2px solid rgba(58, 184, 129, 0.5);
-  background-color: rgba(58, 184, 129, 0.5);
-  border-radius: 4px;
   color: rgb(125, 118, 113);
-  display: flex;
-  justify-content: center;
   font-size: 13px;
   align-items: center;
+  & img {
+    padding: 5px;
+    height: 60px;
+  }
 `;
 
 const ReactNode: FC<any> = (props) => {
   return (
     <React.Fragment>
-      <Container className="container">React-Node</Container>
+      <Container className="container">
+        <img src={props.src} />
+      </Container>
     </React.Fragment>
   );
 };
