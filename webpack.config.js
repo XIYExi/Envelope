29b84543352de24242dcde9aea923aab80d4b9f3
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-05-04 10:16:38 +0800

    跨域

--- webpack.config.js
+++ webpack.config.js
@@ -10,3 +10,16 @@ module.exports = {
     },
   },
 };
+
+/*const {createProxyMiddleware} = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(createProxyMiddleware('/api',
+    {
+      target: "http://localhost:8012",
+      changeOrigin:true,
+      pathRewrite: {
+        "^/api": "/"
+      },
+      "secure":false 	//如果访问的是https类的链接，就需要设置为true
+    },))
+}*/
