eb5ff60358f0b00536f05632fb67943feb26470f
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-05-06 09:20:40 +0800

    添加管理页 登录主页

--- src/pages/rbac/page/Home/index.tsx
+++ src/pages/rbac/page/Home/index.tsx
@@ -1,9 +1,49 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Alert, Tag, Typography } from 'antd';
+import { Divider, Segment } from 'semantic-ui-react';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+const HeadWrapper = styled.div`
+  padding: 1.6em;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+`;
+
+const SegmentWrapper = styled.div`
+  margin: 2em;
+  padding: 1em;
+`;
 
 const Home: FC = (props: any) => {
   return (
     <>
-      <div>RBAC - Home</div>
+      <HeadWrapper>
+        <Typography.Title level={2}>Envelope 管理页</Typography.Title>
+      </HeadWrapper>
+
+      <SegmentWrapper>
+        <Alert type="success" message="欢迎回来，管理员 xiye" closable />
+
+        <Divider hidden />
+
+        <Segment>
+          <Typography.Title level={5}>
+            <Tag icon={<ExclamationCircleOutlined />} color="warning">
+              warning
+            </Tag>
+            请自觉遵守操作规范
+          </Typography.Title>
+        </Segment>
+
+        <Segment>
+          <Typography.Title level={5}>
+            <Tag icon={<ExclamationCircleOutlined />} color="warning">
+              warning
+            </Tag>
+            管理责任重大，修改数据请谨慎！
+          </Typography.Title>
+        </Segment>
+      </SegmentWrapper>
     </>
   );
 };
