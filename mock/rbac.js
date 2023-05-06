123affb3a07f898f78028c42fc9dc9f4b0fcbfc4
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-05-06 09:18:57 +0800

    修改mock数据，统一管理登录信息

--- mock/rbac.js
+++ mock/rbac.js
@@ -7,7 +7,7 @@ export default {
       userId: '1652319467646234622',
       userAvatar:
         'https://gw.alipayobjects.com/mdn/rms_43231b/afts/img/A*W9g1S7W-TOoAAAAAAAAAAAAAARQnAQ',
-      userNickname: '@name',
+      userNickname: 'xiye',
       userUsername: '@name',
       userPhone: '@id(11)',
       userEmail: '@email',
@@ -32,5 +32,62 @@ export default {
         updateTime: '@datetime("yyyy-MM-dd A HH:mm:ss")',
       },
     ],
+  }),
+
+  'GET /rbac/back/queryByUsernameAndRole': mockjs.mock({
+    success: true,
+    data: [
+      {
+        userId: '1652319467646234622',
+        userAvatar:
+          'https://gw.alipayobjects.com/mdn/rms_43231b/afts/img/A*W9g1S7W-TOoAAAAAAAAAAAAAARQnAQ',
+        userNickname: '@name',
+        userUsername: '@name',
+        userPhone: '@id(11)',
+        userEmail: '@email',
+        userRole: 'admin',
+        userDescription: '@cparagraph(3)',
+      },
+    ],
+  }),
+
+  'GET /rbac/back/queryByUsername': mockjs.mock({
+    success: true,
+    data: [
+      {
+        userId: '1652319467646234622',
+        userAvatar:
+          'https://gw.alipayobjects.com/mdn/rms_43231b/afts/img/A*W9g1S7W-TOoAAAAAAAAAAAAAARQnAQ',
+        userNickname: '@name',
+        userUsername: '@name',
+        userPhone: '@id(11)',
+        userEmail: '@email',
+        userRole: 'admin',
+        userDescription: '@cparagraph(3)',
+      },
+    ],
+  }),
+
+  'GET /rbac/back/queryByRole': mockjs.mock({
+    success: true,
+    'data|15': [
+      {
+        userId: '@id',
+        userUsername: '@name',
+        userNickname: '@name',
+        pwd: '@string("lower",3,8)',
+        userPhone: '@id(11)',
+        userEmail: '@email',
+        userRole: 'admin',
+        gender: '@boolean',
+        createTime: '@datetime("yyyy-MM-dd A HH:mm:ss")',
+        updateTime: '@datetime("yyyy-MM-dd A HH:mm:ss")',
+      },
+    ],
+  }),
+
+  'GET /rbac/log/query': mockjs.mock({
+    success: true,
+    'data|5': ['@date() [INFO] @paragraph(1) \n @paragraph(1)'],
   }),
 };
